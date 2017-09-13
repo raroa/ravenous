5539a5733f8850847befe51e534414bbb58d50aa
raroa <darkkcity@yahoo.com>
2017-09-13 17:31:03 +0300

    state added on line 29

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ class App extends React.Component {
       <div className="App">
         <h1>ravenous</h1>
         <SearchBar searchYelp={this.searchYelp} />
-        <BusinessList businesses={this.businesses} />
+        <BusinessList businesses={this.state.businesses} />
       </div>
     )
   }
